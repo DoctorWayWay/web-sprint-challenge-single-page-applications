2dd31f19915738f4e9c0d89c257b05ccf57d54ab
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-15 10:06:39 -0700

    Added 2 inputs in the PizzaForm component along with an image of a pizza

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,6 @@
 // Library Imports
 import React from "react";
-
+import { Link } from "react-router-dom";
 // Component Imports
 import NavBar from "./NavBar";
 
@@ -8,6 +8,19 @@ const HomePage = () => {
   return (
     <div>
       <NavBar />
+      <div>
+        {/* Image will be set to a background image on the div containing the image element */}
+        <img
+          src="/assets/Pizza.jpg"
+          alt="A delicious cheese pizza with some spinach on it."
+        />
+        <h2>Your favorite food, delivered while coding</h2>
+        <button>
+          <Link id="order-pizza" to={"/pizza"}>
+            Build a Pizza!
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -3,7 +3,43 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const PizzaForm = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      {/* Intro to form */}
+      <div>
+        <h2>Build Your Own Pizza</h2>
+      </div>
+      <div>
+        <img
+          src="/assets/Pizza.jpg"
+          alt="A delicious cheese pizza with some spinach on it."
+        />
+      </div>
+      {/* FORM */}
+      <form id="pizza-form">
+        {/* Buyers Name Input */}
+        <label>
+          Name:
+          <input
+            id="name-input"
+            type="text"
+            name="buyer"
+            placeholder="First Name"
+          />
+        </label>
+        {/* Pizza Size Input */}
+        <label>
+          Pizza Size:
+          <select id="size-dropdown" name="pizzaSize">
+            <option value="unSelected">--Choose Pizza Sizing--</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default PizzaForm;
